21025d5f0d955a26da7305fac19f343173e3d54c
Jahirg <67656660+Jahirg@users.noreply.github.com>
2021-11-24 13:20:26 -0500

    Update arduino_mega.ino
    the delay instructions was replaced for count mills.

--- Dashboard/Arduino_code/arduino_mega/arduino_mega.ino
+++ Dashboard/Arduino_code/arduino_mega/arduino_mega.ino
@@ -3,23 +3,41 @@
 // Cualquier aporte, mejora, a mi correo JAHIRG@YAHOO.COM
 // No soy experto en programacion, quiero aprender.
 // Este codigo es desean hace proyectos usando interfaz grafica en pc
+//
+// Example program for a control panel using PYQT QT as a graphical interface.
+// Any contribution, improvement, to my mail JAHIRG@YAHOO.COM
+// I am not an expert in programming, I want to learn.
+// This code is to make projects using graphical interface on PC.
 // ###############################################################################
 
 //######################################################################
-//# LISTA DE REGISTROS A USAR PARA EL ENVIO DE DATOS
+//# LISTA DE REGISTROS USADOS EN EL PROGRAMA
+//# LIST OF REGISTERS USED IN THE PROGRAM
 //######################################################################
 
+// #### Registros de prueba son 16 datos de 16 bits.(0-7 ADC, 8-15 DIN)
+// #### Test registers are 16 data of 16 bits (0-7 ADC, 8-15 DIN).
 int au16data[16] = {
   300, 102, 1003, 104, 10000, 20000, 30000, 32000, 0, 0, 0, 0, 0, 0, 1, 1 };
 const size_t dataLength = 16;
-
+int Period1, Period2;
+unsigned long Time1, Time2;
 
 //######################################################################
 //# DEFINICION DE PINES:
+//# NO USAR INSTRUCCIONES PULL UP, CAUSAN FALLAS EN COMUNICACIONES
+//
+//# PIN CONFIGURATION:
 //# PLEASE DONT USE PULLUP INSTRUCTIONS CAUSE FAULT IN COMMUNICTIONS
 //######################################################################
+
 void setup() {
-  // #### Slider para PMW
+
+  // #### ajustes en comunicacion.
+  Period1 = 200;
+  Period2 = 2;
+  
+  // #### slider para PMW
   pinMode(4,OUTPUT);
   pinMode(5,OUTPUT);
   pinMode(6,OUTPUT);   
@@ -38,7 +56,7 @@ void setup() {
   pinMode(67,INPUT);
   pinMode(68,INPUT);
   pinMode(69,INPUT);
-
+  
   // ### Senales para indicadores
   pinMode(22,OUTPUT);
   pinMode(23,OUTPUT); 
@@ -48,24 +66,24 @@ void setup() {
   pinMode(27,OUTPUT); 
   pinMode(28,OUTPUT);
   pinMode(29,OUTPUT);
-
+  
   Serial.begin( 9600 ); // baud-rate at 9600
-  }
+}
 
 //##################################################################
 //# ENVIO DE DATOS AL PC
+//# SENDING DATA TO THE PC
 //##################################################################
 void sendBytes(uint16_t value)
-  {
+{
   Serial.write(highByte(value));
   Serial.write(lowByte(value));
-  }
-
+}
 //##################################################################
-//# ACTIVACION LEDS
+//# ACTIVACION LEDS H/L
 //##################################################################
-void vProcessaUart(String s)
-  {
+void vProcessaUart(String s){
+  
   if(s == "22H")
     digitalWrite(22,HIGH);
   if(s == "22L")
@@ -106,47 +124,40 @@ void vProcessaUart(String s)
   if(s == "29L")
     digitalWrite(29,LOW);
 
-
 //#####################################################################
 //# SECCION PWM 
 //#####################################################################
-  String pwm =s;
-  int datapwm=0;
-  pwm.remove(0, 3);
-  datapwm=pwm.toInt();
-  s.remove(3);
+    String pwm =s;
+    int datapwm=0;
+    pwm.remove(0, 3);
+    datapwm=pwm.toInt();
+    s.remove(3);
 
-  if(s == "P04")
+ if(s == "P04")
     analogWrite(4, datapwm);
-  
-  if(s == "P05")
+ if(s == "P05")
     analogWrite(5, datapwm);
-  
-  if(s == "P06")
+ if(s == "P06")
     analogWrite(6, datapwm); 
-  
-  if(s == "P07")
+ if(s == "P07")
     analogWrite(7, datapwm); 
-  
-  if(s == "P08")
+ if(s == "P08")
     analogWrite(8, datapwm);
-  
-  if(s == "P09")
+ if(s == "P09")
     analogWrite(9, datapwm);
-  
-  if(s == "P10")
+ if(s == "P10")
     analogWrite(10, datapwm); 
-  if(s == "P11")
+ if(s == "P11")
     analogWrite(11, datapwm);
-  }
-
+}
 
 //#####################################################################
 //# BUCLE PRINCIPAL
+//# MAIN LOOP
 //#####################################################################
 void loop() {
-  
-  //#### Registro 0 al 7 son entradas analogas
+// #### Registros 0 al 7 son entradas analogos
+// #### Registers 0 to 7 are analog inputs
   au16data[0]=analogRead(A0);
   au16data[1]=analogRead(A1);
   au16data[2]=analogRead(A2);
@@ -156,22 +167,23 @@ void loop() {
   au16data[6]=analogRead(A6);
   au16data[7]=analogRead(A7);
 
-  //#### Registro 8 al 15 son entradas digitales
+// #### Registros 8 al 15 son entradas digitales 
+// #### Registers 8 to 15 are digital inputs 
   if(digitalRead(62))
     au16data[8]=621;
   else
     au16data[8]=620;
-
+  
   if(digitalRead(63))
     au16data[9]=631;
   else
     au16data[9]=630;
-
+  
   if(digitalRead(64))
     au16data[10]=641;
   else
     au16data[10]=640;
-
+  
   if(digitalRead(65))
     au16data[11]=651;
   else
@@ -186,33 +198,40 @@ void loop() {
     au16data[13]=671;
   else
     au16data[13]=670;
-
+  
   if(digitalRead(68))
     au16data[14]=681;
   else
     au16data[14]=680;
-
+  
   if(digitalRead(69))
     au16data[15]=691;
   else
     au16data[15]=690;
 
-
-  //#### Envio de datos al PC #########
-  for(int n = 0; n < 16; n++){
-    sendBytes(au16data[n]);
+    
+//#### Envio de datos al PC
+//#### Sending data to the PC
+  if (millis()-Time1 >= Period1) {
+    for(int n = 0; n < 16; n++){
+      sendBytes(au16data[n]);
+    }
+    Time1 = millis();
   }
-
-  //#### Captura de datos del PC ######
+    
+//#### Captura de datos del PC
+//#### PC data capture
   String sz = "";
   char ch;
   int nCmpt = 0;
   while (Serial.available() > 0) {
+    if (millis()-Time2 >= Period2) { 
     sz += (char)Serial.read();
     nCmpt++;
-    delay(2);
+    Time2 = millis(); 
     }
+  }
   if(nCmpt)
-  vProcessaUart(sz);
-  delay(100);
+    vProcessaUart(sz);
+
 }
